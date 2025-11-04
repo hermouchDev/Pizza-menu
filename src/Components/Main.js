16282d93c81d017ddf4eea3c9b56d087a32fddb8
Hermouch Abdelmajid <hermouch.webdev@gmail.com>
2025-11-04 09:41:36 +0100

    style(mobile): center pizza images below at <=480px

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,34 +1,40 @@
-import pizzaData from '../data'
+import pizzaData from "../data";
 function Main() {
     const pizzas = pizzaData;
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <p>Authentic Moroccain cuisine. 6 creative dishes to choose from. All from our stone oven, All organic, All delicious.</p>
-            <ul className='pizzas'>
-                {
-                    pizzas.map((pizza) => <Pizza pizzaInfo={pizza} key={pizza.name}/>)
-                }
+            <p>
+                Authentic Moroccain cuisine. 6 creative dishes to choose from.
+                All from our stone oven, All organic, All delicious.
+            </p>
+            <ul className="pizzas">
+                {pizzas.map((pizza) => (
+                    <Pizza pizzaInfo={pizza} key={pizza.name} />
+                ))}
             </ul>
         </main>
-    )
+    );
 }
 
-function Pizza({pizzaInfo}) {
+function Pizza({ pizzaInfo }) {
     const pizzaStatu = pizzaInfo.soldOut ? "sold-out" : "";
     return (
         <li className={`pizza ${pizzaStatu}`}>
-            <img src={pizzaInfo.photoName} alt="props.pizzaName" />
-            <div>
+            <div className="pizza-image">
+                <img src={pizzaInfo.photoName} alt="props.pizzaName" />
+            </div>
+            <div className="pizza-info">
                 <h3>{pizzaInfo.name}</h3>
                 <p>{pizzaInfo.ingredients}</p>
-                {
-                    pizzaStatu ? <span>Sold Out</span> : <span>{pizzaInfo.price} DH</span>
-                }
+                {pizzaStatu ? (
+                    <span>Sold Out</span>
+                ) : (
+                    <span>{pizzaInfo.price} DH</span>
+                )}
             </div>
         </li>
     );
 }
 
-
-export default Main
+export default Main;
